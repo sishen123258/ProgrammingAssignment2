82dda3d822dd25549c97c0472201c5ce0f389411
sishen123258 <1144299328@qq.com>
2014-12-18 18:02:12 +0800

    your comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R±à³Ì×÷Òµ git push  -f origin master
 
-## Write a short comment describing this function
+## Write a  short comment describing this function
+## I make the inverse matrix contains all NA
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- matrix(NA,nrow(x),ncol(x))
+    set <- function(y){
+        x <<- y
+        inverse <- matrix(NA,nrow(x),ncol(x))
+    }
+    get <- function() x
+    setInverse <- function(ans) inverse <<- ans
+    getInverse <- function() inverse
+    list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
 ## Write a short comment describing this function
+## judge the inverse matrix cached or not by counting the number of NA 
+## the count is larger than zero when the inverse is NULL
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    is_cached <- (sum(!is.na(inverse))>0)
+    if(is_cached){
+        message("getting cached data")
+        return(inverse)
+    }
+    tmp <- x$get()
+    inverse <- solve(tmp)
+    x$setInverse(inverse)
+    inverse
 }
